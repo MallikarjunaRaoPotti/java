ac8681277aff3a5a1c1670540e9d3b0c1441c297
MALLIKARJUNA RAO POTTI <151558503+MallikarjunaRaoPotti@users.noreply.github.com>
2024-09-21 23:11:38 +0530

    Add files via upload

---
+++ PrimitiveDatatypes.java
@@ -0,0 +1,24 @@
+public class PrimitiveDatatypes {
+    public static void main(String[] args){
+        // Primitive Datatype
+        int a=789;
+        System.out.println("int"+a);
+        byte b=127;
+        System.out.println("byte"+b);
+        short s=546;
+        System.out.println("short"+s);
+        long l=23454534344343l;
+        System.out.println("long"+l);
+        Double d=56.77;
+        System.out.println("double"+d);
+        float f=23.56f;
+        System.out.println("float"+f);
+        char c='f';
+        System.out.println("char"+c);
+       
+        
+
+    }
+    
+    
+}
