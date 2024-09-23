ea8cd86439e4529d6e0d2d2d63cf4296982e2750
MALLIKARJUNA RAO POTTI <151558503+MallikarjunaRaoPotti@users.noreply.github.com>
2024-09-23 11:30:57 +0530

    Add files via upload

---
+++ Switchcases.java
@@ -0,0 +1,15 @@
+
+public class Switchcases {
+    public static void main(String[] args){
+        int day=3;
+        switch(day){
+            case 1:System.out.println("monday");break;
+            case 2:System.out.println("tuesday");break;
+            case 3:System.out.println("wednesday");break;
+            case 4:System.out.println("thursday");break;
+            default:System.out.println("anthor one");
+    }
+}
+
+    
+}
